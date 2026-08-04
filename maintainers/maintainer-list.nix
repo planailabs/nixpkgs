094358b005f24015da9b0402d1f0b7265d2cbace
Sebastian Friedrich <35170732+sebfried@users.noreply.github.com>
2026-08-04 16:57:38 +0200

    maintainers: update sebfried

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25548,7 +25548,7 @@
     keys = [ { fingerprint = "0AF6 4C3B 1F12 14B3 8C8C  5786 1FA2 DBE6 7438 7CC3"; } ];
   };
   sebfried = {
-    name = "Sebastian Friedrich";
+    name = "Sebastian W. Friedrich";
     github = "sebfried";
     githubId = 35170732;
   };
